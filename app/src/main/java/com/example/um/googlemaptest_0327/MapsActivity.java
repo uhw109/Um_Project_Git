339cf13057f9812cbc7ecdd5e727965dac5c8e9a
Um <uhw109@naver.com>
2018-04-05 15:07:13 +0900

    ddo

--- app/src/main/java/com/example/um/googlemaptest_0327/MapsActivity.java
+++ app/src/main/java/com/example/um/googlemaptest_0327/MapsActivity.java
@@ -24,6 +24,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Log.d("test", "Git Manual");
         Log.d("test", "It's Hostfix Branch");
         Log.d("test", "It's Master Branch");
+        Log.d("test", "ddo");
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
